27addaea40bcccb83202d55664145a6308795c10
tusharsingh8 <tushki.shahi@gmail.com>
2017-10-01 04:16:10 +0530

    wrong time

--- app/routes.js
+++ app/routes.js
@@ -20,25 +20,7 @@ module.exports = function(app, passport) {
   
         else
             {   
-                //console.log(req.sessionStore.sessions);
-                console.log("----");
-                //console.log(req.sessionStore.sessions.cookie);
-                console.log("----");
                 var errormessage = req.flash("error");
-                console.log("----");
-                console.log(req.flash("error"));                
-                console.log(req.session['flag']);
-                if(errormessage[0])
-                {
-                console.log("error message");
-                console.log(errormessage);
-                }
-                else
-                {
-               
-                console.log("error message not there");
-               console.log(errormessage); 
-                }
                 res.render('homepage.ejs',{error : errormessage//function()
                     //{
                          //   while(true);
@@ -61,100 +43,19 @@ module.exports = function(app, passport) {
     }));
 
   
-  /*  app.post('/signup', function(req, res) {
-       passport.authenticate('local',function(err,user,info)
-      {
-        console.log("sss");
-        User.findOne({ 'local.username' :  user.username }, function(err, founduser) {
-
-            // if there are any errors, return the error
-            if (err)
-
-            {             console.log("asdasdasd1");
-                return next(err);
-            }
-            // check to see if theres already a user with that email
-            if (founduser) {
-            console.log("asdasdasd2");
-                if(founduser.username == user.username)
-                {  res.render('homepage.ejs',{error: "That username is already in use."});   }
-                else if(founduser.delegatecard == req.user.delegatecard)
-                { 
-                      res.render('homepage.ejs',{error: "That delegate card is already in use."}); 
-                }
-            }
-            else
-            {
-                            console.log("asdasdasd3");
-                // if there is no user with that email
-                // create the user
-                
-                var newUser            = new User();
-                // set the user's local credentials
-                newUser.local.username    = username;
-                newUser.local.password = newUser.generateHash(password);
-                newUser.delegatecard = req.body.delegatecard;
-                newUser.phonenumber = req.body.phonenumber;
-                newUser.level = 1;
-         
-                // save the user
-                newUser.save(function(err) {
-                    if (err)
-                        throw err;
-                    res.render('game.ejs');
-                });
-            }(req,res); 
-        });    
-
-       
-       
-       }); }); 
-    
-    
- */   
-    
-    
-
     // =====================================
     // GAME ================================
     // =====================================
 
     app.get('/game',isLoggedIn,function(req,res)
     {
-            
-//            console.log(req.session);
-
-//            console.log(req.session.flag);
-        //if(!req.session.flag) flag = null;
-        //else flag = req.session.flag;
-
-        //console.log("FLAG IS " + req.session['flag']);
-        //console.log("flag is " + flag);
-       /* if(req.query.flag)
-        {
-            console.log('flag is there');
-            console.log(req.query.flag);
-        } */
-        //req.session.flag = null;
         flag = req.session['flag'];
         req.session['flag'] = 0;
         if(req.user)
         {
             //var foundquestion = "kutta";
         Scenario.findOne({'number' : req.user.scenario},function(err, scenario) {
-            // if there are any errors, return the error
-          /*  if (err){
-              console.log("nahi mil gaya");
-                return done(err); }
-            // check to see if theres already a user with that email
-            else { */
-             // console.log("mil gaya");
-           //   console.log(scenario);
               foundquestion = scenario.questions[req.user.level-1];
-            //} 
-            // });
-  //          console.log(foundquestion);
-            //  console.log(flash('error'));
             res.render('game.ejs',
         {
             user : req.user,
@@ -180,12 +81,6 @@ module.exports = function(app, passport) {
         {
 
             Scenario.findOne({'number' : req.user.scenario},function(err, scenario) {
-            // if there are any errors, return the error
-           // console.log(foundquestion);
-            //          console.log(flash('error'));
-         //   console.log("submitted answer " + req.body.submittedanswer);
-           // console.log("actual answer " + scenario.answers[req.user.level-1]);
-            //console.log(req.body.submitanswer);
             if(req.body.submitanswer == 'Skip')
             {
                 flag = 0;
@@ -197,13 +92,8 @@ module.exports = function(app, passport) {
             }
             else
             {
-              //  console.log(req.body);
-             //   console.log("wrong answer");
                 flag = -1;
             }
-        //console.log(res);
-        // look and update the score of the player
-        // if the the question was the last question change scenario
             if(req.user.level == 10)
                 {            
 
@@ -212,8 +102,6 @@ module.exports = function(app, passport) {
                 function(err,user)
                 {
                     if(err) throw err;
-                //console.log(user);
-                //console.log("updated");
                     req.session['flag'] = flag;
                     res.redirect('/game');
             
@@ -242,21 +130,6 @@ module.exports = function(app, passport) {
 
     });
 
-/*    app.get('/changescenario',isLoggedin,function(req,res){
-        if(req.user)
-        {
-        User.findOneAndUpdate(
-        {'delegatecard' : req.user.delegatecard},{$inc : {delegatecard: 1}},function(err,user)
-        {
-          console.log(user);
-        });
-        }
-        else
-        {
-            res.redirect('/');
-        }
-        });
-*/
 
 // ===================== 
 // ==== LEADER BOARD ==== 
@@ -267,17 +140,11 @@ app.get('/leaderboard', function(req, res) {
     User.find().sort({points : -1}).limit(10).exec(function(err,docs)
                                 { 
             var array = [];
-  //          console.log(docs);
             docs.forEach(function(user)
             {
-                
-            console.log(user.points);
               array.push({name : user.local.username, points : user.points, delegatecard : user.delegatecard });
-                console.log("-----");                
       });      
 
-                console.log("-----");     
-   console.log(array);    
     res.render('leaderboard.ejs',{users: array}); 
                                 });             
 
